8cc48ab01385864b8a4c83fd719fe0becf3f0cf7
zenith110 <thequake@live.com>
2023-09-07 20:37:43 -0400

    Added cuisine file to grab list of availabile cuisines

--- src/backend/fastapi-server/main.py
+++ src/backend/fastapi-server/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
-
+from os import environ
+import requests
+import random
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+@app.get("/food/")
+async def food_retrival():
+    api_key = environ.get("FOODAPIKEY")
+    size = ""
+    cusines = []
+    with open("cuisine.txt", "r") as file1:
+        read_content = file1.read()
+        cusines = read_content.split()
+    cusine = random.choice(cusines)
+    url = f"https://api.spoonacular.com/recipes/complexSearch?cuisine={cusine}&number=2&addRecipeNutrition=true&addRecipeInformation=true&instructionsRequired=true&apiKey={api_key}"
+    request = requests.get(url).json()
+    return request
